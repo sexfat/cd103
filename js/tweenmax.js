15c0a75bd3baedc34dc47d5e178168304ecb4335
sexfat <bryant6831@gmail.com>
2018-10-06 13:58:39 +0800

    add

--- js/tweenmax.js
+++ js/tweenmax.js
@@ -1,14 +1,24 @@
 console.log("ok");
-TweenMax.to(".box01", .8, {
+TweenMax.to(".box01", 4, {
     x: 200,
     y: 100,
     ease: Elastic.easeOut,
-    delay: 2
+    delay: 2,
+    width: 100,
+    scale: 1.9,
+    // alpha: 0,
+    // repeat: 2,
+    // repeatDelay : 1,
+    rotation: 360,
+    borderRadius:"100px"
 });
 
 
 TweenMax.from(".box02", .8, {
     y: 200,
-    ease: SlowMo.ease.config(0.7, 0.7, false),
-    delay: 4
+    // ease: SlowMo.ease.config(0.7, 0.7, false),
+    delay: 4,
+    alpha: 0,
+   
+
 });
