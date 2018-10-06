16438f5bf60476d75491ff6d0eaeb0a66c151bce
sexfat <bryant6831@gmail.com>
2018-10-06 14:19:37 +0800

    add yoyo

--- js/tweenmax.js
+++ js/tweenmax.js
@@ -1,24 +1,46 @@
 console.log("ok");
-TweenMax.to(".box01", 4, {
-    x: 200,
-    y: 100,
-    ease: Elastic.easeOut,
+
+function animaion_01() {
+    TweenMax.to(".box01", 4, {
+        x: 200,
+        y: 100,
+        ease: Elastic.easeOut,
+        delay: 2,
+        // width: 100,
+        // scale: 1.9,
+        // alpha: 0,
+        // repeat: 2,
+        // repeatDelay : 1,
+        // rotation: 360,
+        // borderRadius: "100px"
+    });
+
+
+    TweenMax.from(".box02", .8, {
+        y: 200,
+        // ease: SlowMo.ease.config(0.7, 0.7, false),
+        delay: 4,
+        alpha: 0,
+
+    });
+
+    TweenMax.set(".box03", {
+        y: 200,
+        x: 10
+    });
+};
+
+// animaion_01();
+
+
+TweenMax.fromTo(".box04", 2, {
+    y: -10
+}, {
+    y: 150,
     delay: 2,
-    width: 100,
-    scale: 1.9,
-    // alpha: 0,
-    // repeat: 2,
-    // repeatDelay : 1,
-    rotation: 360,
-    borderRadius:"100px"
-});
-
-
-TweenMax.from(".box02", .8, {
-    y: 200,
-    // ease: SlowMo.ease.config(0.7, 0.7, false),
-    delay: 4,
-    alpha: 0,
-   
+    repeatDelay : 1,
+    // ease: Elastic.easeOut,
+    repeat: -1,
+    yoyo :false
 
 });
