86a85c71bdc4a17165a2d93af63e18587563a5ee
sexfat <bryant6831@gmail.com>
2018-10-06 12:00:50 +0800

    add from

--- js/tweenmax.js
+++ js/tweenmax.js
@@ -1,2 +1,14 @@
 console.log("ok");
-TweenMax.to(".box01" ,.8 ,{x:200,ease:Elastic.easeOut});
+TweenMax.to(".box01", .8, {
+    x: 200,
+    y: 100,
+    ease: Elastic.easeOut,
+    delay: 2
+});
+
+
+TweenMax.from(".box02", .8, {
+    y: 200,
+    ease: SlowMo.ease.config(0.7, 0.7, false),
+    delay: 4
+});
